5d6e88e86106cb8afe007c85558f9a4af1a9309f
Sophia Katsuno <sophia.katsuno@gmail.com>
2024-06-14 23:14:50 -0700

    edit background/initial settings

--- src/App.vue
+++ src/App.vue
@@ -7,5 +7,14 @@ export default {};
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira Sans', sans-serif;
+}
 
+body {
+  background: #EEE;
+}
 </style>
